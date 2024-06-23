503170d12e55c3f4d555b1cdc9bae543aa512222
Adam McElroy <adam.mcelroy15@gmail.com>
2024-06-23 02:21:03 +0100

    implemented finite state machine for player movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,33 +1,18 @@
+class_name Player
 extends CharacterBody2D
 
 @export var speed = 100
-@onready var animated_sprite = $AnimatedSprite2D
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+@onready var fsm = $FiniteStateMachine as FiniteStateMachine
+@onready var player_idle_state = $FiniteStateMachine/PlayerIdleState as PlayerIdleState
+@onready var player_walk_state = $FiniteStateMachine/PlayerWalkState as PlayerWalkState
 
-func play_animation():
-	
-	if Input.is_action_pressed("left"):
-		animated_sprite.flip_h = true
-		animated_sprite.play("walk")
-	
-	if Input.is_action_pressed("right"):
-		animated_sprite.flip_h = false
-		animated_sprite.play("walk")
-	
-	if Input.is_action_pressed("up"):
-		animated_sprite.play("walk")
-	
-	if Input.is_action_pressed("down"):
-		animated_sprite.play("walk")
-	
-	if Input.is_action_just_released("left") or Input.is_action_just_released("right") or Input.is_action_just_released("up") or Input.is_action_just_released("down"):
-		animated_sprite.play("idle")
 
-func _physics_process(delta):
-	get_input()
-	play_animation()
-	move_and_slide()
 
+func _ready():
+	# player idle signals
+	player_idle_state.start_movement.connect(fsm.change_state.bind(player_walk_state))
+	
+	# player walk signals
+	player_walk_state.stop_movement.connect(fsm.change_state.bind(player_idle_state))
+	
---
+++ scripts/states/state.gd
@@ -0,0 +1,9 @@
+class_name State
+extends Node
+
+func _enter_state() -> void:
+	pass
+	
+func _exit_state() -> void:
+	pass
+
