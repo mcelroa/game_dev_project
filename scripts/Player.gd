fcaaf31859e8cf05d2855518e60297aa7c355dc2
Adam McElroy <adam.mcelroy15@gmail.com>
2024-07-31 16:23:18 +0100

    restart

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,18 +1,11 @@
-class_name Player
 extends CharacterBody2D
 
 @export var speed = 100
 
-@onready var fsm = $FiniteStateMachine as FiniteStateMachine
-@onready var player_idle_state = $FiniteStateMachine/PlayerIdleState as PlayerIdleState
-@onready var player_walk_state = $FiniteStateMachine/PlayerWalkState as PlayerWalkState
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
-
-
-func _ready():
-	# player idle signals
-	player_idle_state.start_movement.connect(fsm.change_state.bind(player_walk_state))
-	
-	# player walk signals
-	player_walk_state.stop_movement.connect(fsm.change_state.bind(player_idle_state))
-	
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
--- scripts/states/state.gd
+++
@@ -1,9 +0,0 @@
-class_name State
-extends Node
-
-func _enter_state() -> void:
-	pass
-	
-func _exit_state() -> void:
-	pass
-
